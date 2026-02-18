72a4d4bb9128f691685a977c8708e989290e40ac
IvanMay <IvanMay44@yandex.ru>
2026-02-18 11:19:02 +0300

    feat: Start sctript work
    Start script work, create scripts Player, MainMenu, Settings, implement player move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public const float maxHP = 100f;
+    public const float speed = 4.4f;
+    public float currentHP { get; private set; }
+    public float physicDamage { get; private set; }
+    public float mageDamage { get; private set; }
+
+    new Rigidbody rigidbody;
+
+    public static Player instance;
+
+    void Awake() {
+        instance = this;
+        rigidbody = GetComponent<Rigidbody>();
+        physicDamage = 10f;
+        mageDamage = 15f;
+        currentHP = maxHP;
+    }
+
+    void FixedUpdate() {
+        Move();
+        if (Input.GetKey(Settings.physicAttackKey)) PhysicAttack();
+        else if (Input.GetKey(Settings.mageAttackKey)) MageAttack();
+    }
+
+    void Move() {
+        float runEffect = Input.GetKey(Settings.runKey) ? 2f : 1f;
+        float speedX = (Input.GetKey(Settings.leftwardKey) ? -speed : Input.GetKey(Settings.rightwardKey) ? speed : 0) * runEffect;
+        float speedZ = (Input.GetKey(Settings.forwardKey) ? speed : Input.GetKey(Settings.backwardKey) ? -speed : 0) * runEffect;
+        float coefficient = (speedX != 0 && speedZ != 0) ? Mathf.Sqrt(2) : 1;
+        rigidbody.linearVelocity = new Vector3(speedX / coefficient, rigidbody.linearVelocity.y, speedZ / coefficient);
+    }
+
+    void PhysicAttack() {
+        Debug.Log("Test text phisic attack");
+    }
+
+    void MageAttack() {
+        Debug.Log("Test text mage attack");
+    }
+
+    public void GetDamage(float damage) {
+        currentHP = Mathf.Max(0, currentHP - damage);
+        if (currentHP < 0) Death();
+    }
+
+    void Death() {
+        Debug.Log("Test text death player");
+        MainMenu.instance.GameOver();
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,12 @@
+public static class Settings {
+    public static string forwardKey = "w";
+    public static string leftwardKey = "a";
+    public static string backwardKey = "s";
+    public static string rightwardKey = "d";
+    public static string runKey = "left shift";
+    public static string physicAttackKey = "mouse 0";
+    public static string mageAttackKey = "mouse 1";
+
+    public static float musicVolume = 1f;
+    public static float soundVolume = 1f;
+}
